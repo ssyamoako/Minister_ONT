1fa156c9afd674bd60d9118e790899f4cadb72cf
M i N i S T E R <51422644+ssyamoako@users.noreply.github.com>
2023-08-01 09:41:33 +0000

    Update guppy_basecaller.sh

--- guppy_basecaller.sh
+++ guppy_basecaller.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#PBS -l nodes=1:ppn=36
+#PBS -l mem=64GB
+#PBS -l ngpus=2
+#PBS -l walltime=500:00:00
 
 base=/home2/ssyamoako/Edem_Raw/20221014_1953_MN25545_FAT64681_2b24c46e
 run=${base}/ont-guppy-cpu/bin/
